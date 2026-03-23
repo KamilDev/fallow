266105b51386eae04156d87325b8bbf184df658c
github-action-benchmark <github@users.noreply.github.com>
2026-03-23 10:17:12 +0000

    add Fallow Allocations (customSmallerIsBetter) benchmark result for eb707bfebfe7aba7168b56eca51af00e4ee64ecb

--- dev/allocs/data.js
+++ dev/allocs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774260873068,
+  "lastUpdate": 1774261032166,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Allocations": [
@@ -87,6 +87,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak Allocations",
             "value": 3673,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "eb707bfebfe7aba7168b56eca51af00e4ee64ecb",
+          "message": "fix: add missing type_only_dependencies field in snapshot test",
+          "timestamp": "2026-03-23T11:15:50+01:00",
+          "tree_id": "3a196e9777ef417ef043e1da34a35fde455dd63b",
+          "url": "https://github.com/fallow-rs/fallow/commit/eb707bfebfe7aba7168b56eca51af00e4ee64ecb"
+        },
+        "date": 1774261031656,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Total Bytes Allocated",
+            "value": 3002275,
+            "unit": "bytes"
+          },
+          {
+            "name": "Total Allocations",
+            "value": 11577,
+            "unit": "allocations"
+          },
+          {
+            "name": "Peak Memory",
+            "value": 401746,
+            "unit": "bytes"
+          },
+          {
+            "name": "Peak Allocations",
+            "value": 3669,
             "unit": "allocations"
           }
         ]
