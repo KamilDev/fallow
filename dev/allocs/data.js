e5b428d8ae8195f7496020f0aaf88c85064b4056
github-action-benchmark <github@users.noreply.github.com>
2026-03-24 14:27:35 +0000

    add Fallow Allocations (customSmallerIsBetter) benchmark result for 60af661085bbc0e3207180679b882773fa00d197

--- dev/allocs/data.js
+++ dev/allocs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774356923635,
+  "lastUpdate": 1774362454995,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Allocations": [
@@ -879,6 +879,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "Peak Allocations",
             "value": 3978,
+            "unit": "allocations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "60af661085bbc0e3207180679b882773fa00d197",
+          "message": "chore: release v1.7.0",
+          "timestamp": "2026-03-24T15:23:48+01:00",
+          "tree_id": "086b0c428fa093968631ea159046eae448eb662e",
+          "url": "https://github.com/fallow-rs/fallow/commit/60af661085bbc0e3207180679b882773fa00d197"
+        },
+        "date": 1774362454620,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Total Bytes Allocated",
+            "value": 3036897,
+            "unit": "bytes"
+          },
+          {
+            "name": "Total Allocations",
+            "value": 12148,
+            "unit": "allocations"
+          },
+          {
+            "name": "Peak Memory",
+            "value": 419434,
+            "unit": "bytes"
+          },
+          {
+            "name": "Peak Allocations",
+            "value": 5168,
             "unit": "allocations"
           }
         ]
