90554d0adf84906a5eafe47271ac6a9aabd78aee
github-action-benchmark <github@users.noreply.github.com>
2026-03-24 14:29:54 +0000

    add Fallow Binary Size (customSmallerIsBetter) benchmark result for 60af661085bbc0e3207180679b882773fa00d197

---
+++ dev/bloat/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1774362594812,
+  "repoUrl": "https://github.com/fallow-rs/fallow",
+  "entries": {
+    "Fallow Binary Size": [
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "60af661085bbc0e3207180679b882773fa00d197",
+          "message": "chore: release v1.7.0",
+          "timestamp": "2026-03-24T15:23:48+01:00",
+          "tree_id": "086b0c428fa093968631ea159046eae448eb662e",
+          "url": "https://github.com/fallow-rs/fallow/commit/60af661085bbc0e3207180679b882773fa00d197"
+        },
+        "date": 1774362594426,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size (fallow)",
+            "value": 114448816,
+            "unit": "bytes"
+          }
+        ]
+      }
+    ]
+  }
+}
