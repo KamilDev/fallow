06bef3b1f6fc83c90faff1a075ac69b2eacb6403
github-action-benchmark <github@users.noreply.github.com>
2026-03-25 08:53:14 +0000

    add Fallow Coverage (customBiggerIsBetter) benchmark result for c479b3f59c31c6ac203f19e8ac04c956fb27188c

--- dev/coverage/data.js
+++ dev/coverage/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774428592249,
+  "lastUpdate": 1774428794001,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Coverage": [
@@ -1038,6 +1038,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/fallow-rs/fallow/commit/851e9a2ec31f34155064750d2be1fd1c94a21fd3"
         },
         "date": 1774428591910,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "Code Coverage",
+            "value": 90.2,
+            "unit": "%"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "c479b3f59c31c6ac203f19e8ac04c956fb27188c",
+          "message": "fix: qualify Span in benchmark to resolve ambiguous import",
+          "timestamp": "2026-03-25T09:51:51+01:00",
+          "tree_id": "2bf39f35c8b234f291084a777104795c11dd36df",
+          "url": "https://github.com/fallow-rs/fallow/commit/c479b3f59c31c6ac203f19e8ac04c956fb27188c"
+        },
+        "date": 1774428793491,
         "tool": "customBiggerIsBetter",
         "benches": [
           {
