8fcc0e6e28cd1c97700a74505c7a885f601bc576
github-action-benchmark <github@users.noreply.github.com>
2026-03-27 08:36:14 +0000

    add Fallow Binary Size (customSmallerIsBetter) benchmark result for 22ee80595999ee8784e93520d18f1c761accf564

--- dev/bloat/data.js
+++ dev/bloat/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774597470788,
+  "lastUpdate": 1774600574724,
   "repoUrl": "https://github.com/fallow-rs/fallow",
   "entries": {
     "Fallow Binary Size": [
@@ -898,6 +898,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Binary Size (fallow)",
             "value": 120888272,
+            "unit": "bytes"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "committer": {
+            "email": "bart@waardenburg.dev",
+            "name": "Bart Waardenburg",
+            "username": "BartWaardenburg"
+          },
+          "distinct": true,
+          "id": "22ee80595999ee8784e93520d18f1c761accf564",
+          "message": "Improve code quality and harden watch reloads",
+          "timestamp": "2026-03-27T09:33:16+01:00",
+          "tree_id": "488046ee4e9dfd8066d522d088c12d74f7ceb002",
+          "url": "https://github.com/fallow-rs/fallow/commit/22ee80595999ee8784e93520d18f1c761accf564"
+        },
+        "date": 1774600574185,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Binary Size (fallow)",
+            "value": 120887776,
             "unit": "bytes"
           }
         ]
